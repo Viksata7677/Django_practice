608686f8a18a411a4eaa25b387ebbf0483373099
Viktor Iordanov <viki1204394@gmail.com>
2024-12-12 17:29:33 +0200

    Add: Profile details logic

--- DjangoTastyRecipesApp/profiles/urls.py
+++ DjangoTastyRecipesApp/profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from profiles.views import ProfileCreateView
+from profiles.views import ProfileCreateView, ProfileDetailView
 
 urlpatterns = [
     path('create/', ProfileCreateView.as_view(), name='create-profile'),
+    path('details/', ProfileDetailView.as_view(), name='profile-details'),
 ]
--- DjangoTastyRecipesApp/profiles/views.py
+++ DjangoTastyRecipesApp/profiles/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
+
+from DjangoTastyRecipesApp.utility import get_user_obj
 from profiles.forms import ProfileCreateForm
 from profiles.models import Profile
 
@@ -12,3 +14,11 @@ class ProfileCreateView(CreateView):
     template_name = "create-profile.html"
     form_class = ProfileCreateForm
     success_url = reverse_lazy('catalogue')
+
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = 'details-profile.html'
+
+    def get_object(self, queryset=None):
+        return get_user_obj()
